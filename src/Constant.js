5a86d54847566b347cad36d3c54234195d582f9c
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2024-06-23 14:42:54 +0530

    host url changes

--- src/Constant.js
+++ src/Constant.js
@@ -1,2 +1,2 @@
-export const HOST_URL = "https://youtube-backend-1-nhiw.onrender.com";
+export const HOST_URL = "https://youtube-backend2002.onrender.com";
 // export const HOST_URL = "http://localhost:8000";
